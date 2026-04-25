606490b29e380fb755e98c253b015fe6cb3d11ef
Lucas <lucasamarosouza1@gmail.com>
2026-04-25 12:56:05 -0300

    calculadora simples

--- Aula09/Program.cs
+++ Aula09/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("===Operadores Lógicos===");
+
+        bool isLogged = true;
+        bool isAdmin = false;
+
+        Console.WriteLine("Informações do usuário:");
+        Console.WriteLine("Usuário logado: " + isLogged);
+        Console.WriteLine("Acesso de administrador: " + isAdmin);
+
+        Console.WriteLine("\nPermissões");
+
+        //Operador OR (||)
+        if (isLogged || isAdmin)
+        {
+            Console.WriteLine("Acesso concedido.");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado.");
+        }
+
+        //Operador AND (&&)
+        if (isLogged && isAdmin)
+        {
+            Console.WriteLine("Acesso ao painel de administração concedido.");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao painel de administraçã negado.");
+        }
+
+        //Operador NOT (!)
+        if (!isLogged)
+        {
+            Console.WriteLine("Usuário não logado.");
+        }
+        else
+        {
+            Console.WriteLine("Usuário logado.");
+        }
+
+    }
+}
---
+++ Aula10/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Aula10;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        bool varContinue = true;
+
+        while (varContinue){
+            Console.WriteLine("===Calculadora Simples===\n");
+
+            Console.WriteLine("Digite o primeiro número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Selecione o tipo de operação
+
+            Console.WriteLine("Escolha uma operação:");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("Digite o número da operação: ");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            double res = 0;
+            //Condições
+            if (operation == 1)
+            {
+                res = num1 + num2;
+            }
+            else if (operation == 2)
+            {
+                res = num1 - num2;
+            }
+            else if (operation == 3)
+            {
+                res = num1 * num2;
+            }
+            else if (operation == 4)
+            {
+                if (num2 != 0)
+                {
+                    res = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisão por zero não é permitida.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Operação inválida.");
+            }
+            Console.WriteLine("\n O resultado é: " + res + "\n");
+
+            Console.WriteLine("\n Deseja realizar outra operação? (s/n)\n");
+
+            string response = Console.ReadLine();
+
+            if (response != "s")
+            {
+                varContinue = false;
+            }
+        }
+
+        Console.WriteLine("Obrigado por usar a calculadora! Até a próxima.");
+
+    }
+}
